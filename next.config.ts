8c148e65f969af142e29ac971a8f9fb80bca77df
Nhan Phan <57086421+nhanphan0411@users.noreply.github.com>
2026-07-22 17:11:35 +0700

    update: config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "pub-e1ce6cd7aa9640ccb36c096dbb0c14c7.r2.dev" },
+      { protocol: "https", hostname: process.env.R2_PUBLIC_HOSTNAME! },
     ],
   },
 };
